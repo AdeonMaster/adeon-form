024a99a668c4a3ef2038f50c9571433e4a738a69
AdeonMaster <adeonmaster@gmail.com>
2019-10-11 00:39:25 +0300

    Fixed radio input issues, minor changes

--- src/form/Field.js
+++ src/form/Field.js
@@ -5,69 +5,98 @@ import PropTypes from 'prop-types';
 
 import withForm from './withForm';
 
+const getDefaultValue = (type, value, defaultValue, defaultChecked) => {
+  switch (type) {
+    case 'checkbox':
+      return defaultChecked || false;
+
+    case 'radio':
+      return defaultChecked
+        ? value
+        : '';
+
+    default:
+      return defaultValue || '';
+  }
+};
+
 const Field = ({
-  context, name, type, defaultValue, defaultChecked, children, component, ...otherProps
+  context, name, type, value, defaultValue, defaultChecked, children, component, ...otherProps
 }) => {
+  const inputValue = useMemo(
+    () => {
+      const { state } = context;
+
+      return state[name] !== undefined
+        ? state[name]
+        : getDefaultValue(type, value, defaultValue, defaultChecked);
+    },
+    [context, name, type, value, defaultChecked, defaultValue]
+  );
+
   const handleChange = useCallback(
     event => {
       const { updateField } = context;
-      const value = type === 'checkbox'
+      const inputValue = type === 'checkbox'
         ? event.target.checked
         : event.target.value;
 
-      updateField(name, value);
+      updateField(name, inputValue);
     },
     [context, name, type]
   );
 
+  const props = useMemo(
+    () => {
+      const commonProps = {
+        ...otherProps,
+        name,
+        type,
+        onChange: handleChange,
+      };
+
+      switch (type) {
+        case 'checkbox':
+          return {
+            ...commonProps,
+            checked: inputValue
+          };
+
+        case 'radio':
+          return {
+            ...commonProps,
+            value,
+            checked: inputValue === value
+          };
+
+        default:
+          return {
+            ...commonProps,
+            value: inputValue
+          };
+      }
+    },
+    [name, type, value, otherProps, inputValue, handleChange]
+  );
+
   useEffect(
     () => {
-      const { updateField, state } = context;
-
-      const value = type === 'checkbox'
-        ? (defaultChecked || false)
-        : (defaultValue || '');
+      const { state } = context;
+      const inputValue = getDefaultValue(type, value, defaultValue, defaultChecked);
 
       // mutate the context state trick
-      state[name] = value;
+      if (
+        (type !== 'radio')
+        || (type === 'radio' && state[name] === undefined)
+        || (type === 'radio' && state[name] !== value && defaultChecked)
+      ) {
+        state[name] = inputValue;
+      }
 
-      updateField(name, value);
+      // updateField(name, inputValue);
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     []
-  );
-
-  const value = useMemo(
-    () => {
-      const { state } = context;
-
-      return state[name] || (
-        type === 'checkbox'
-          ? (defaultChecked || false)
-          : (defaultValue || '')
-      );
-    },
-    [context, name, type, defaultChecked, defaultValue]
-  );
-
-  const props = useMemo(
-    () => (
-      type === 'checkbox'
-        ? {
-          ...otherProps,
-          name,
-          type,
-          onChange: handleChange,
-          checked: value
-        }
-        : {
-          ...otherProps,
-          name,
-          type,
-          onChange: handleChange,
-          value
-        }
-    ),
-    [name, type, otherProps, value, handleChange]
   );
 
   if (typeof (children) === 'function') {
